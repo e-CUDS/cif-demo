1fec80691daaa3ef52ff8b52d609193c88eee730
Jesper Friis <jesper.friis@sintef.no>
2017-03-06 20:26:42 +0100

    added compatibility with newer versions of PyCifRW and python3

--- cifdata.py
+++ cifdata.py
@@ -23,15 +23,17 @@ CifData = softpy.entity('cifdata', '0.1', 'http://emmc.info/meta')
 cifdata = CifData()
 
 # Initialise the instance from the cif file
+cifkeys = [k.lower() for k in rut.keys()]  # newer versions of PyCifRW
+                                           # changed all keys to lower-case
 for name in cifdata.soft_get_property_names():
-    tag = '_' + name
-    if tag in rut.keys():
+    tag = '_' + name.lower()
+    if tag in cifkeys:
         value = rut[tag]
     elif name == 'atom_site_type_symbol':
         value = [re.match('[A-Z][a-z]{0,2}', l).group()
                  for l in rut['_atom_site_label']]
     else:
-        raise KeyError('cannot derive "%s" from cif data')
+        raise KeyError('cannot derive "%s" from cif data' % name)
     cifdata.soft_set_property(name, value)
 
 # TODO
@@ -41,12 +43,27 @@ for name in cifdata.soft_get_property_names():
 # - Map cifdata to corresponding CUDS instance (or the other way around)
 # - Try to see formalise this mapping
 
-#---------
+
+#----------------
+# Some testing...
+#----------------
+import numpy as np
+
+cifdata.symmetry_Int_Tables_number = np.int32('34')
 
 # Print content of cifdata
+print()
 for name in cifdata.soft_get_property_names():
     print('%38s = %-r' % (name, getattr(cifdata, name)))
+print('-' * 60)
 
 # Store the cifdata to hdf5 file
 with softpy.Storage(driver='hdf5', uri='rutile.h5') as s:
     s.save(cifdata)
+
+# Reload and print the cifdata
+cifdata2 = CifData(uuid=cifdata.soft_get_id(), driver='hdf5',
+                   uri='rutile.h5')
+for name in cifdata2.soft_get_property_names():
+    print('%38s = %-r' % (name, getattr(cifdata2, name)))
+print('-' * 60)
